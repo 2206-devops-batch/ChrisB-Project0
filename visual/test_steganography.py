4884bcce658ac49475961ba3e5f30fe5c013feff
Christopher Barnes <chris.barnes.2000@me.com>
2022-06-14 12:16:45 -0700

    Squashed commit of the following:
    commit a2890dfe69262c2df35da562aac1ed8610cd69d1
    Author: Christopher Barnes <chris.barnes.2000@me.com>
    Date:   Tue Jun 14 09:43:28 2022 -0700
    
        Add Doc Strings To Test Scaffold
    
    commit a4ce6a3156a5efdc3dcc52ce6b616263759d9e8a
    Author: Christopher Barnes <chris.barnes.2000@me.com>
    Date:   Tue Jun 14 09:17:22 2022 -0700
    
        Fix test_sum_tuple - got 5 expected 6
    
    commit 8947bc7a3e0ad54347bccf42f224fa56191ed81f
    Author: Christopher Barnes <chris.barnes.2000@me.com>
    Date:   Tue Jun 14 09:15:11 2022 -0700
    
        Add Testing Scaffold For Encoding Images
    
    commit 050db34c4fb7d5e691f2ab0bb196766a46476d27
    Author: Christopher Barnes <chris.barnes.2000@me.com>
    Date:   Tue Jun 14 08:53:32 2022 -0700
    
        Add Placeholders

--- visual/test_steganography.py
+++ visual/test_steganography.py
@@ -3,26 +3,45 @@ import unittest
 
 class TestSum(unittest.TestCase):
 
-    def test_sum(self):
+    def test_sum_list(self):
+        """
+        Test that it can sum a list of integers
+        """
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
     def test_sum_tuple(self):
+        """
+        Test that it can sum a tuple of integers
+        """
         self.assertEqual(sum((1, 2, 3)), 6, "Should be 6")
 
 
 class TestEndcodeImage(unittest.TestCase):
 
-  def test_open_image(self):
-    pass
+    def test_open_image(self):
+        """
+        Test that it can open a image file based on a give path
+        """
+        pass
 
-  def test_getting_image_properties(self):
-    pass
+    def test_getting_image_properties(self):
+        """
+        Test that it can output useful data about our opened image
+        """
+        pass
 
-  def test_create_new_image(self):
-    pass
+    def test_create_new_image(self):
+        """
+        Test that it can create a new GRB image & load/set it to the same size as our input image
+        """
+        pass
 
-  def test_embeding_image(self):
-    pass
+    def test_embeding_image(self):
+        """
+        Test that it can ...
+        """
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
